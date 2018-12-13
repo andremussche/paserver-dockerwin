36596fc1d19b16a5eaeff22d53551317d2634926
André Mussche <andre.mussche@gmail.com>
2018-12-13 13:43:52 +0100

    Add files via upload

---
+++ paserver-update.bat
@@ -0,0 +1,3 @@
+cd \
+del "\Program Files (x86)\Embarcadero\PAServer\19.0\paserver.exe"
+copy \paserver\paserver.exe "/Program Files (x86)/Embarcadero/PAServer/19.0/paserver.exe"
---
+++ run.bat
@@ -0,0 +1,7 @@
+REM docker run -i -h paserverwindows paserverwindows
+docker run -d -h paserverwindows paserverwindows
+pause
+
+REM Delphi 10.2.3 -> Platform Assistant Server  Version 10.2.1.13
+REM Connection refused. Platform Assistant Server version mismatch - expecting version '10.3.1.15'
+REM https://cc.embarcadero.com/item/30837
